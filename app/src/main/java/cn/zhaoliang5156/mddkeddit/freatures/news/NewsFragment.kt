16c859046c551f2d5a74d29039a767419544388c
zhaoliang <2668645098@qq.com>
2017-09-28 16:32:20 +0800

    添加 delegated properties

--- app/src/main/java/cn/zhaoliang5156/mddkeddit/freatures/news/NewsFragment.kt
+++ app/src/main/java/cn/zhaoliang5156/mddkeddit/freatures/news/NewsFragment.kt
@@ -3,12 +3,14 @@ package cn.zhaoliang5156.mddkeddit.freatures.news
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
 import cn.zhaoliang5156.mddkeddit.R
 import cn.zhaoliang5156.mddkeddit.common.extensions.inflate
+import kotlinx.android.synthetic.main.fragment_news.*
 
 
 /**
@@ -16,11 +18,20 @@ import cn.zhaoliang5156.mddkeddit.common.extensions.inflate
  */
 class NewsFragment : Fragment() {
 
+    private val newsList by lazy {
+        news_list
+    }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return container?.inflate(R.layout.fragment_news)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        newsList.setHasFixedSize(true)
+        newsList.layoutManager = LinearLayoutManager(context)
     }
 
 }// Required empty public constructor
